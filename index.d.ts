98dc85c6c35a98398a32b07365cbf3ff13fd3a9c
VitorMac10 <vitormac3@hotmail.com>
2020-04-27 12:16:13 -0300

    Removed worker_threads

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,13 @@
-import { Writable } from "stream";
 import { ChildProcess } from "child_process";
-import { MessagePort } from "worker_threads";
 
 declare namespace StreamUtils {
 
-    /**
-     * The instance of ffmpeg with default [audio/webm - codec opus] output
-     */
-    var ffmpeg: ChildProcess;
-
-    declare class WorkerStream extends Writable {
+    declare class ffmpeg {
 
         /**
-         * Creates a new Writable stream to be used with 'worker_threads'
-         * @param port The worker's parent port
+         * Creates a new instance of ffmpeg with default [audio/webm - codec opus] output
          */
-        constructor(port: MessagePort);
-
-        /**
-         * Writes data to the worker's parent port
-         * @param data The buffer data to be written
-         */
-        write(data: Buffer): void;
+        static create(): ChildProcess;
 
     }
 
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-const { Writable } = require('stream');
 const { spawn } = require('child_process');
 
 const ffmpeg_static = require('ffmpeg-static');
 const args = ['-f', 'mp3', '-i', 'pipe:0', '-acodec', 'libopus', '-f', 'webm', 'pipe:1'];
 
-class WorkerStream extends Writable {
+class ffmpeg {
 
-    constructor(port) {
-        super({ autoDestroy: true });
-        this.port = port;
-    }
-
-    write(data) {
-        this.port.postMessage(data.buffer);
+    static create() {
+        return spawn(ffmpeg_static, args);
     }
 
 }
 
 module.exports = {
-    get ffmpeg() {
-        return spawn(ffmpeg_static, args);
-    },
-    WorkerStream
+    ffmpeg
 }
